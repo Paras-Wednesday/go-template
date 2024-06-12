ed798178473e3bfa931de8d3133f9d28f827a795
Paras Wednesday <paras@wednesday.is>
2024-06-12 16:18:19 +0530

    feat: seeders for `authors` and `posts`

---
+++ cmd/seeder/v4/4_posts.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go-template/cmd/seeder/utls"
+	"go-template/internal/config"
+	"go-template/internal/postgres"
+	"go-template/models"
+	"log"
+
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+)
+
+func main() {
+	err := config.LoadEnv()
+	if err != nil {
+		log.Println(err)
+	}
+	db, err := postgres.Connect()
+	if err != nil {
+		log.Fatal(err, "pop")
+	}
+	author, err := models.Authors(qm.OrderBy("id")).One(context.Background(), db)
+	if err != nil {
+		log.Fatalf("Couldn't query author: %s", err)
+	}
+	_ = utls.SeedData("posts", fmt.Sprintf(`
+	INSERT INTO public.posts(author_id, content)
+	VALUES (%d, 'This is my first post');`, author.ID))
+}

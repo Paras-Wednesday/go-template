025f830d0a9855d3ce59b984ee06bd89f071d29d
Paras Wednesday <paras@wednesday.is>
2024-06-17 14:38:12 +0530

    feat: add daos for `Authors` with tests
    - Add test util for Mock Author data

---
+++ daos/authors.go
@@ -0,0 +1,53 @@
+package daos
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+
+	"go-template/models"
+)
+
+func CreateAuthorTx(ctx context.Context, author models.Author, tx *sql.Tx) (models.Author, error) {
+	contextExecutor := GetContextExecutor(tx)
+
+	err := author.Insert(ctx, contextExecutor, boil.Infer())
+	return author, err
+}
+
+func CreateAuthor(ctx context.Context, author models.Author) (models.Author, error) {
+	return CreateAuthorTx(ctx, author, nil)
+}
+
+func UpdateAuthor(ctx context.Context, author models.Author) (models.Author, error) {
+	contextExecutor := GetContextExecutor(nil)
+
+	_, err := author.Update(ctx, contextExecutor, boil.Infer())
+	return author, err
+}
+
+func DeleteAuthor(ctx context.Context, author models.Author) (int64, error) {
+	contextExecutor := GetContextExecutor(nil)
+
+	return author.Delete(ctx, contextExecutor)
+}
+
+func FindAuthorByID(ctx context.Context, id int) (*models.Author, error) {
+	contextExecutor := GetContextExecutor(nil)
+	return models.FindAuthor(ctx, contextExecutor, id)
+}
+
+func FindAuthorByFirstName(ctx context.Context, fname string) (*models.Author, error) {
+	contextExecutor := GetContextExecutor(nil)
+	return models.Authors(qm.Where(fmt.Sprintf("%s=$1", models.AuthorColumns.FirstName), fname)).
+		One(ctx, contextExecutor)
+}
+
+func FindAuthorByLastName(ctx context.Context, lname string) (*models.Author, error) {
+	contextExecutor := GetContextExecutor(nil)
+	return models.Authors(qm.Where(fmt.Sprintf("%s=$1", models.AuthorColumns.LastName), lname)).
+		One(ctx, contextExecutor)
+}
---
+++ daos/authors_test.go
@@ -0,0 +1,274 @@
+package daos_test
+
+import (
+	"context"
+	"database/sql/driver"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"go-template/daos"
+	"go-template/models"
+	"go-template/testutls"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateAuthor(t *testing.T) {
+	tests := []struct {
+		name    string
+		author  models.Author
+		wantErr bool
+	}{
+		{
+			name: "should create a author",
+			author: models.Author{
+				FirstName: testutls.MockAuthor().FirstName,
+				LastName:  testutls.MockAuthor().LastName,
+			},
+			wantErr: false,
+		},
+	}
+
+	mock, cleanup, _ := testutls.SetupMockDB(t)
+	defer cleanup()
+
+	// As the insertion will return the "id", and "deleted_at"
+	// column which are non default column
+	rows := sqlmock.NewRows([]string{
+		models.AuthorColumns.ID,
+		models.AuthorColumns.DeletedAt,
+	}).AddRow(
+		testutls.MockAuthor().ID,
+		testutls.MockAuthor().DeletedAt,
+	)
+
+	mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO "authors"`)).
+		WithArgs().
+		WillReturnRows(rows)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			gotAuthor, err := daos.CreateAuthor(context.Background(), test.author)
+			assert.Equal(t, test.wantErr, err != nil,
+				"wantErr: %t, got: %v", test.wantErr, err,
+			)
+			assert.Equal(t, test.author.FirstName, gotAuthor.FirstName)
+			assert.Equal(t, test.author.LastName, gotAuthor.LastName)
+		})
+	}
+}
+
+func TestFindAuthorByID(t *testing.T) {
+	tests := []struct {
+		name    string
+		userID  int
+		wantErr bool
+		init    func(mock sqlmock.Sqlmock)
+	}{
+		{
+			name:    "Should return author with id 1",
+			userID:  1,
+			wantErr: false,
+			init: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{models.AuthorColumns.ID}).AddRow(testutls.MockAuthor().ID)
+				mock.ExpectQuery(regexp.QuoteMeta(
+					`select * from "authors" where "id"=$1`,
+				)).WithArgs().WillReturnRows(rows)
+			},
+		},
+		{
+			name:    "Should return error on finding user",
+			userID:  1,
+			wantErr: true,
+			init: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery(regexp.QuoteMeta(
+					`select * from "authors" where "id"=$1`,
+				)).WithArgs().WillReturnError(
+					fmt.Errorf("couldn't find user"),
+				)
+			},
+		},
+	}
+
+	mock, cleanup, _ := testutls.SetupMockDB(t)
+	defer cleanup()
+
+	for _, test := range tests {
+		test.init(mock)
+		t.Run(test.name, func(t *testing.T) {
+			author, err := daos.FindAuthorByID(context.Background(), test.userID)
+			assert.Equal(t, test.wantErr, err != nil,
+				"wantErr: %t got: %v", test.wantErr, err)
+			if author != nil {
+				assert.Equal(t, test.userID, author.ID)
+			}
+		})
+	}
+}
+
+func TestFindAuthorByFirstName(t *testing.T) {
+	tests := []struct {
+		testName  string
+		firstName string
+		wantErr   bool
+		init      func(mock sqlmock.Sqlmock)
+	}{
+		{
+			testName:  "Should return error in finding author",
+			firstName: "abc",
+			wantErr:   true,
+			init: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery(regexp.QuoteMeta(
+					`SELECT "authors".* FROM "authors"
+					 WHERE (first_name=$1) LIMIT 1;`)).
+					WithArgs().
+					WillReturnError(fmt.Errorf("No such user"))
+			},
+		},
+		{
+			testName:  "Should return author with first_name \"abc\"",
+			firstName: "abc",
+			wantErr:   false,
+			init: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{
+					models.AuthorColumns.ID,
+					models.AuthorColumns.FirstName,
+				}).AddRow(
+					testutls.MockAuthor().ID,
+					testutls.MockAuthor().FirstName,
+				)
+				mock.ExpectQuery(regexp.QuoteMeta(
+					`SELECT "authors".* FROM "authors"
+					 WHERE (first_name=$1) LIMIT 1;`,
+				)).
+					WithArgs().
+					WillReturnRows(rows)
+			},
+		},
+	}
+
+	mock, cleanup, _ := testutls.SetupMockDB(t)
+	defer cleanup()
+
+	for _, test := range tests {
+		test.init(mock)
+		t.Run(test.testName, func(t *testing.T) {
+			author, err := daos.FindAuthorByFirstName(context.Background(), test.firstName)
+			assert.Equal(t, test.wantErr, err != nil,
+				"wantErr: %t, got: %v", test.wantErr, err)
+			t.Logf("author: %+v", author)
+		})
+	}
+}
+
+func TestFindAuthorByLastName(t *testing.T) {
+	tests := []struct {
+		testName string
+		lastName string
+		wantErr  bool
+		init     func(mock sqlmock.Sqlmock)
+	}{
+		{
+			testName: "Should return error in finding author",
+			lastName: "abc",
+			wantErr:  true,
+			init: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery(regexp.QuoteMeta(
+					`SELECT "authors".* FROM "authors"
+					 WHERE (last_name=$1) LIMIT 1;`,
+				)).
+					WithArgs().
+					WillReturnError(fmt.Errorf("No such user"))
+			},
+		},
+		{
+			testName: "Should return author with last_name \"abc\"",
+			lastName: "abc",
+			wantErr:  false,
+			init: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{
+					models.AuthorColumns.ID,
+					models.AuthorColumns.LastName,
+				}).AddRow(
+					testutls.MockAuthor().ID,
+					testutls.MockAuthor().LastName,
+				)
+				mock.ExpectQuery(regexp.QuoteMeta(
+					`SELECT "authors".* FROM "authors" WHERE (last_name=$1) LIMIT 1;`,
+				)).
+					WithArgs().
+					WillReturnRows(rows)
+			},
+		},
+	}
+
+	mock, cleanup, _ := testutls.SetupMockDB(t)
+	defer cleanup()
+
+	for _, test := range tests {
+		test.init(mock)
+		t.Run(test.testName, func(t *testing.T) {
+			author, err := daos.FindAuthorByLastName(context.Background(), test.lastName)
+			t.Logf("author: %+v", author)
+			assert.Equal(t, test.wantErr, err != nil,
+				"wantErr: %t, got: %v", test.wantErr, err)
+		})
+	}
+}
+
+func TestUpdateAuthor(t *testing.T) {
+	cases := []struct {
+		name    string
+		req     models.Author
+		wantErr bool
+	}{
+		{
+			name:    "Should update Author",
+			req:     models.Author{},
+			wantErr: false,
+		},
+	}
+	mock, cleanup, _ := testutls.SetupMockDB(t)
+	defer cleanup()
+	for _, test := range cases {
+		result := driver.Result(driver.RowsAffected(1))
+		mock.ExpectExec(regexp.QuoteMeta(`UPDATE "authors"`)).
+			WillReturnResult(result)
+
+		t.Run(test.name, func(t *testing.T) {
+			_, err := daos.UpdateAuthor(context.Background(), test.req)
+			assert.Equal(t, test.wantErr, err != nil,
+				"wantErr: %t, got: %v", test.wantErr, err)
+		})
+	}
+}
+
+func TestDeleteAuthor(t *testing.T) {
+	cases := []struct {
+		name    string
+		req     models.Author
+		wantErr bool
+	}{
+		{
+			name:    "Should delete author",
+			req:     models.Author{},
+			wantErr: false,
+		},
+	}
+	mock, cleanup, _ := testutls.SetupMockDB(t)
+	defer cleanup()
+	for _, test := range cases {
+		result := driver.Result(driver.RowsAffected(1))
+		mock.ExpectExec(regexp.QuoteMeta(
+			`DELETE FROM "authors" WHERE "id"=$1`,
+		)).
+			WillReturnResult(result)
+		t.Run(test.name, func(t *testing.T) {
+			_, err := daos.DeleteAuthor(context.Background(), test.req)
+			assert.Equal(t, test.wantErr, err != nil,
+				"wantErr: %t, got: %v", test.wantErr, err)
+		})
+	}
+}

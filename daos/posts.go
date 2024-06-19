2dae5b2b41267c4024e53383dda91ddb3c9accfd
Paras-Wednesday <paras@wednesday.is>
2024-06-19 11:23:37 +0530

    feat: add post daos with test cases

---
+++ daos/posts.go
@@ -0,0 +1,64 @@
+package daos
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+
+	"go-template/models"
+)
+
+func CreatePostTx(ctx context.Context, post models.Post, tx *sql.Tx) (models.Post, error) {
+	contextExecutor := GetContextExecutor(tx)
+
+	err := post.Insert(ctx, contextExecutor, boil.Infer())
+	return post, err
+}
+
+func CreatePost(ctx context.Context, post models.Post) (models.Post, error) {
+	return CreatePostTx(ctx, post, nil)
+}
+
+func UpdatePostTx(ctx context.Context, post models.Post, tx *sql.Tx) (models.Post, error) {
+	contextExecutor := GetContextExecutor(tx)
+
+	_, err := post.Update(ctx, contextExecutor, boil.Infer())
+	return post, err
+}
+
+func UpdatePost(ctx context.Context, post models.Post) (models.Post, error) {
+	return UpdatePostTx(ctx, post, nil)
+}
+
+func DeletePostTx(ctx context.Context, post models.Post, tx *sql.Tx) (int64, error) {
+	contextExecutor := GetContextExecutor(tx)
+
+	return post.Delete(ctx, contextExecutor)
+}
+
+func DeletePost(ctx context.Context, post models.Post) (int64, error) {
+	return DeletePostTx(ctx, post, nil)
+}
+
+func FindPostByID(ctx context.Context, id int) (*models.Post, error) {
+	contextExecutor := GetContextExecutor(nil)
+	return models.FindPost(ctx, contextExecutor, id)
+}
+
+func FindAllPostBylAuthorWithCount(ctx context.Context, authorID int, queries ...qm.QueryMod) (models.PostSlice, int64, error) {
+	contextExecutor := GetContextExecutor(nil)
+
+	authQueryMode := models.PostWhere.AuthorID.EQ(authorID)
+	count, err := models.Posts(authQueryMode).Count(ctx, contextExecutor)
+	if err != nil {
+		return models.PostSlice{}, 0, err
+	}
+
+	// accumulate all the queries including the author ID filter
+	queries = append(queries, authQueryMode)
+
+	posts, err := models.Posts(queries...).All(ctx, contextExecutor)
+	return posts, count, err
+}

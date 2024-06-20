0adbfa7421782db3ce46395633f540762eb1f6da
Paras-Wednesday <paras@wednesday.is>
2024-06-20 17:47:11 +0530

    fix: the seeder script for author insertion

--- cmd/seeder/v3/3_authors.go
+++ cmd/seeder/v3/3_authors.go
@@ -1,11 +1,18 @@
 package main
 
-import "go-template/cmd/seeder/utls"
+import (
+	"fmt"
+	"go-template/cmd/seeder/utls"
+	"go-template/pkg/utl/secure"
+)
 
 func main() {
-	_ = utls.SeedData("authors", `INSERT INTO public.authors
-	(first_name, last_name, created_at) VALUES
-		('John', 'Doe', NOW()),
-		('Jane', 'Doe', NOW());`,
+	sec := secure.New(1, nil)
+	query := "INSERT INTO public.authors" +
+		" (first_name, last_name, created_at, email, password) VALUES" +
+		" ('John', 'Doe', NOW(),'johndoe@mail.com','%s')," +
+		" ('Jane', 'Doe', NOW(),'janedoe@mail.com','%s');"
+	_ = utls.SeedData("authors", fmt.Sprintf(
+		query, sec.Hash("password"), sec.Hash("password")),
 	)
 }
